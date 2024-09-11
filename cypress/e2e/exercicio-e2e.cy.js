86192709aaa11c715ee4c982d1fc85cb6c980842
Guilherme <gulhermeha77@outlook.com>
2024-09-10 23:34:14 -0300

    Adição de produtos ao carrinho automatizado

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -15,10 +15,31 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        /*Login*/
         cy.get('.icon-user-unfollow').click() // Entre na página de login
         cy.login(perfil.usuario,perfil.senha) // Faça login
         cy.contains('Olá').should('exist') // Verifique se o login foi feito
-        
+
+        /*Primeiro Produto*/
+        cy.escolherEAddProdutoAoCarrinho(2, 32, 'Blue', 1)  //Acesse a página de produtos, selecione um e depois escolha uma cor, tamanho e quantidade para ele
+        cy.contains('foi adicionado no seu carrinho').should('exist')
+
+        /*Segundo Produto*/
+        cy.escolherEAddProdutoAoCarrinho(8, 'S', 'Blue', 4) //Acesse a página de produtos, selecione o segundo e depois escolha uma cor, tamanho e quantidade para ele
+        cy.contains('foram adicionados no seu carrinho').should('exist')
+
+        /*Terceiro Produto*/
+        cy.escolherEAddProdutoAoCarrinho(3, 'XL', 'Red', 2) //Acesse a página de produtos, selecione o terceiro e depois escolha uma cor, tamanho e quantidade para ele
+        cy.contains('foram adicionados no seu carrinho').should('exist')
+
+        /*Quarto Produto*/
+        cy.escolherEAddProdutoAoCarrinho(1, 'M', 'Yellow', 1)
+        cy.contains('foi adicionado no seu carrinho').should('exist')
+    
+        /*Carrinho e Checkout*/
+        cy.get('.woocommerce-message > .button').click() // Clique em Ver carrinho
+        cy.get('.checkout-button').click() //Clique em Concluir Compra
+
 
     });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,11 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('escolherEAddProdutoAoCarrinho', (id, tamanho, cor, qtd) => {
+    cy.get('#primary-menu > .menu-item-629 > a').click() // Acesse a página de produtos
+    cy.get('.product-block').eq(id).click() // Escolha um produto
+    cy.get('.variable-items-wrapper').find(`[data-wvstooltip="${tamanho}"]`).click(); //Escolha o tamanho dele
+    cy.get('.variable-items-wrapper').find(`[data-wvstooltip="${cor}"]`).click(); //Escolha a cor dele
+    cy.get('.input-text').clear().type(qtd) //Escolha a quantidade dele
+    cy.get('.single_add_to_cart_button').click() //Clique em Comprar
+});
